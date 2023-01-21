a189fbe71262bc2f26a0d67da79d0d7d1de2be84
付志强 <zhiqiangfu6@gmail.com>
2023-01-21 22:48:29 +0800

    feat: object-oriented

---
+++ src/example/object-oriented.ts
@@ -0,0 +1,29 @@
+class Person {
+    name: any;
+    constructor(name: any) {
+        this.name = name;
+    }
+    say() {
+        console.log('hello sunny')
+    }
+}
+export class Sunny1 extends Person {
+    constructor() {
+        super('fzq1')
+    }
+    love() {
+        console.log('coding1')
+    }
+}
+export class Sunny2 extends Person {
+    constructor() {
+        super('fzq2')
+    }
+    love() {
+        console.log('coding2')
+    }
+}
+const sunny1 = new Sunny1()
+const sunny2 = new Sunny2()
+console.log('[ sunny1.name ] >', sunny1.name)
+console.log('[ sunny2.name ] >', sunny2.name)
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,1 @@
-export const a = 1;
+import './example/object-oriented'
